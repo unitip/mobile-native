a525b47a0e0f9f181fe5ac91253415cfb2615c42
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-14 23:44:07 +0700

    chore: improve chat screen ui

--- app/src/main/java/com/unitip/mobile/features/chat/presentation/ChatScreen.kt
+++ app/src/main/java/com/unitip/mobile/features/chat/presentation/ChatScreen.kt
@@ -1,13 +1,24 @@
 package com.unitip.mobile.features.chat.presentation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -22,9 +33,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -33,15 +45,19 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun ChatScreen(
     onNavigateBack: () -> Unit,
-){
+) {
     var title by remember { mutableStateOf("Rijal Anggoro") }
     var message by remember { mutableStateOf(TextFieldValue("")) }
-    val dummyMessages = listOf(
-        Pair("Rizal Anggooro", "Hi, how are you?"),
-        Pair("You", "I'm good, thanks! How about you?"),
-        Pair("Rizal Anggooro", "I'm doing great, thanks for asking!"),
-        Pair("You", "Glad to hear that!"),
-    )
+    var dummyMessages by remember {
+        mutableStateOf(
+            listOf(
+                Pair("Rizal Anggooro", "Hi, how are you?"),
+                Pair("You", "I'm good, thanks! How about you?"),
+                Pair("Rizal Anggooro", "I'm doing great, thanks for asking!"),
+                Pair("You", "Glad to hear that!"),
+            )
+        )
+    }
 
     Scaffold(
         topBar = {
@@ -59,12 +75,13 @@ fun ChatScreen(
                 modifier = Modifier
                     .padding(paddingValues)
                     .fillMaxSize()
+                    .imePadding()
             ) {
                 // Chat Messages List
                 LazyColumn(
                     modifier = Modifier.weight(1f),
                     contentPadding = PaddingValues(16.dp),
-                    reverseLayout = true // So that new messages appear at the bottom
+                    reverseLayout = false // So that new messages appear at the bottom
                 ) {
                     items(dummyMessages.size) { index ->
                         val (sender, text) = dummyMessages[index]
@@ -75,9 +92,12 @@ fun ChatScreen(
                 // Message Input Field
                 Row(
                     modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp)
                         .fillMaxWidth()
-                        .padding(8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.surfaceContainerHighest),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     TextField(
                         value = message,
@@ -85,19 +105,26 @@ fun ChatScreen(
                         placeholder = { Text("Type a message...") },
                         maxLines = 1,
                         singleLine = true,
-                        colors = TextFieldDefaults.textFieldColors(
-                            containerColor = MaterialTheme.colorScheme.surface,
-                            focusedIndicatorColor = MaterialTheme.colorScheme.primary,
-                            unfocusedIndicatorColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+                        colors = TextFieldDefaults.colors(
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
                         ),
                         modifier = Modifier
                             .weight(1f)
-                            .padding(end = 8.dp)
                     )
 
-                    IconButton(
-                        onClick = { /* Add functionality to send the message */ },
-                        modifier = Modifier.align(Alignment.CenterVertically)
+                    FilledIconButton(
+                        onClick = {
+                            // Send the message
+                            dummyMessages.toMutableList().add(Pair("You", message.text))
+                            message = TextFieldValue("")
+                        },
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(end = 4.dp)
+                            .size(48.dp)
                     ) {
                         Icon(Icons.Default.Send, contentDescription = "Send")
                     }
@@ -125,9 +152,11 @@ fun MessageBubble(sender: String, text: String) {
                     shape = RoundedCornerShape(16.dp)
                 )
                 .padding(12.dp)
-                .align(if (isCurrentUser) {
-                    Alignment.TopEnd
-                } else Alignment.TopStart)
+                .align(
+                    if (isCurrentUser) {
+                        Alignment.TopEnd
+                    } else Alignment.TopStart
+                )
         )
     }
 }
@@ -135,5 +164,5 @@ fun MessageBubble(sender: String, text: String) {
 @Preview(showBackground = true)
 @Composable
 fun ChatScreenPreview() {
-    ChatScreen( onNavigateBack = {})
+    ChatScreen(onNavigateBack = {})
 }
