4a37c84bab7bccc687a22434f78f00984d60d887
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-15 21:12:52 +0700

    feat: dashboard ui

--- app/src/main/java/com/unitip/mobile/core/navigation/HomeNavigationGraph.kt
+++ app/src/main/java/com/unitip/mobile/core/navigation/HomeNavigationGraph.kt
@@ -5,7 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.unitip.mobile.features.chat.screens.ChatsScreen
-import com.unitip.mobile.features.dashboard.presentation.DashboardScreen
+import com.unitip.mobile.features.dashboard.screens.DashboardScreen
 import com.unitip.mobile.features.job.screens.JobsScreen
 import com.unitip.mobile.features.offer.screens.OffersScreen
 import com.unitip.mobile.features.setting.presentation.ProfileScreen
@@ -17,7 +17,7 @@ fun HomeNavigationGraph(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Routes.Home.Offers,
+        startDestination = Routes.Home.Dashboard,
     ) {
         composable<Routes.Home.Dashboard> { DashboardScreen() }
         composable<Routes.Home.Jobs> {
---
+++ app/src/main/java/com/unitip/mobile/features/dashboard/screens/Screen.kt
@@ -0,0 +1,84 @@
+package com.unitip.mobile.features.dashboard.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DashboardScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Dashboard") }
+            )
+        }
+    ) {
+        LazyColumn(modifier = Modifier.padding(it)) {
+            // driver active counter
+            item {
+                Card(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp),
+                    colors = CardDefaults.outlinedCardColors(
+                        containerColor = MaterialTheme.colorScheme.tertiary,
+                    ),
+                ) {
+                    Row(
+                        modifier = Modifier.padding(16.dp),
+                        verticalAlignment = Alignment.Top,
+                    ) {
+                        Text(
+                            text = "12",
+                            style = MaterialTheme.typography.headlineMedium,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Column {
+                            Text(
+                                text = "Driver sedang aktif",
+                                style = MaterialTheme.typography.titleMedium,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                text = "Segera pesan jasa atau ikut penawaran yang mereka tawarkan!",
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.alpha(.8f)
+                            )
+                        }
+                    }
+                }
+            }
+
+            // active order
+            item {
+                Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)) {
+                    Text(
+                        text = "Pesanan Anda",
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                    Text(
+                        text = "Berikut beberapa pesanan Anda yang sedang aktif",
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+            }
+        }
+    }
+}
