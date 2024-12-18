4f2f1cc1a11e534e2679b347d82e4ebaf735a284
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-18 18:42:18 +0700

    fix: change session repository to session manager

--- app/src/main/java/com/unitip/mobile/MainActivity.kt
+++ app/src/main/java/com/unitip/mobile/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
 import androidx.navigation.compose.rememberNavController
-import com.unitip.mobile.shared.data.repositories.SessionRepository
+import com.unitip.mobile.shared.data.managers.SessionManager
 import com.unitip.mobile.shared.presentation.navigation.ApplicationNavigationGraph
 import com.unitip.mobile.shared.presentation.ui.theme.UnitipTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,16 +15,13 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @Inject
-    lateinit var sessionRepository: SessionRepository
+    lateinit var sessionManager: SessionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val isAuthenticated = sessionRepository.read().fold(
-            ifLeft = { false },
-            ifRight = { true }
-        )
+        val isAuthenticated = sessionManager.read() != null
 
         setContent {
             UnitipTheme(
