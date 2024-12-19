bd0065d0fdabb06cabe65636e40a31d3e5bef417
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-19 08:56:43 +0700

    fix: status bar foreground color

--- app/src/main/java/com/unitip/mobile/shared/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/unitip/mobile/shared/presentation/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.unitip.mobile.shared.presentation.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -39,6 +43,12 @@ fun UnitipTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val view = LocalView.current
+    SideEffect {
+        val window = (view.context as Activity).window
+        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+    }
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
