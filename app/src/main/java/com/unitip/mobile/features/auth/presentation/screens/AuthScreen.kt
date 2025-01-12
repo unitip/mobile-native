b6842dd020977483fe6a3fc01b814a72036c31c4
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-12 14:23:40 +0700

    refactor: change AuthRoutes to sealed class and enhance AuthScreen layout

--- app/src/main/java/com/unitip/mobile/features/auth/presentation/screens/AuthScreen.kt
+++ app/src/main/java/com/unitip/mobile/features/auth/presentation/screens/AuthScreen.kt
@@ -1,15 +1,12 @@
 package com.unitip.mobile.features.auth.presentation.screens
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -27,9 +24,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.unitip.mobile.R
@@ -39,7 +36,6 @@ import com.unitip.mobile.features.auth.presentation.viewmodels.AuthViewModel
 import com.unitip.mobile.features.home.core.HomeRoutes
 import com.unitip.mobile.shared.commons.compositional.LocalNavController
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AuthScreen(
     viewModel: AuthViewModel = hiltViewModel(),
@@ -90,45 +86,45 @@ fun AuthScreen(
     }
 
     Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = if (uiState.isLogin) "Masuk"
-                        else "Registrasi"
-                    )
-                },
-            )
-        },
         snackbarHost = { SnackbarHost(hostState = snackbarHost) }
     ) {
         Column(
             modifier = Modifier
-                .padding(it)
                 .fillMaxSize()
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            MaterialTheme.colorScheme.surfaceContainerHigh,
+                            MaterialTheme.colorScheme.surfaceContainerLowest,
+                        )
+                    )
+                )
+                .padding(it)
         ) {
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = if (uiState.isLogin) "Masuk" else "Registrasi",
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(
+                    text = if (uiState.isLogin) "Selamat datang kembali di aplikasi Unitip! Silahkan masukkan beberapa informasi berikut untuk masuk ke akun Unitip Anda"
+                    else "Masukkan beberapa informasi berikut untuk bergabung dan menjadi bagian dari keluarga Unitip!",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
             AnimatedVisibility(visible = uiState.detail is AuthState.Detail.Loading) {
                 LinearProgressIndicator(
                     strokeCap = StrokeCap.Round,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 8.dp)
+                        .padding(start = 16.dp, end = 16.dp, top = 16.dp)
                 )
-                Spacer(modifier = Modifier.height(24.dp))
             }
-
-            Text(
-                text = when (uiState.isLogin) {
-                    true -> "Selamat datang kembali di aplikasi Unitip! Silahkan masukkan beberapa informasi berikut untuk masuk ke akun Unitip Anda"
-                    false -> "Masukkan beberapa informasi berikut untuk bergabung dan menjadi bagian dari keluarga Unitip!"
-                },
-                modifier = Modifier
-                    .padding(start = 32.dp, end = 32.dp),
-                style = with(MaterialTheme.typography.bodyMedium) {
-                    copy(color = MaterialTheme.colorScheme.outline)
-                },
-                textAlign = TextAlign.Center,
-            )
 
             // form
             Column(
