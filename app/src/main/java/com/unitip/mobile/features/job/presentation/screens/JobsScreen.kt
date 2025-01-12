3eaaf0fcdf3250758e9ec97b6bd05c08b475ee7b
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-12 15:19:15 +0700

    refactor: introduce CustomIconButton and enhance JobListItem layout with expandable details

--- app/src/main/java/com/unitip/mobile/features/job/presentation/screens/JobsScreen.kt
+++ app/src/main/java/com/unitip/mobile/features/job/presentation/screens/JobsScreen.kt
@@ -4,39 +4,51 @@ import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.composables.icons.lucide.Lucide
-import com.composables.icons.lucide.RefreshCcw
+import com.composables.icons.lucide.RefreshCw
 import com.unitip.mobile.features.job.core.JobRoutes
 import com.unitip.mobile.features.job.presentation.components.JobListItem
 import com.unitip.mobile.features.job.presentation.states.JobsState
 import com.unitip.mobile.features.job.presentation.viewmodels.JobsViewModel
 import com.unitip.mobile.shared.commons.compositional.LocalNavController
 import com.unitip.mobile.shared.commons.extensions.isCustomer
+import com.unitip.mobile.shared.presentation.components.CustomIconButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,6 +57,8 @@ fun JobsScreen(
 ) {
     val navController = LocalNavController.current
     val context = LocalContext.current
+
+    val listState = rememberLazyListState()
 
     val uiState by viewModel.uiState.collectAsState()
     val isLoading = uiState.detail is JobsState.Detail.Loading
@@ -62,24 +76,26 @@ fun JobsScreen(
     }
 
     Scaffold(
-        contentWindowInsets = WindowInsets(0.dp),
-        modifier = Modifier.padding(bottom = 0.dp),
-        topBar = {
-            TopAppBar(
-                modifier = Modifier.background(Color.Red),
-                title = {
-                    Text("Jobs")
-                },
-                actions = {
-                    IconButton(onClick = { viewModel.refreshJobs() }) {
-                        Icon(
-                            Lucide.RefreshCcw,
-                            contentDescription = null
-                        )
-                    }
-                }
-            )
-        },
+        contentWindowInsets = ScaffoldDefaults.contentWindowInsets.only(
+            WindowInsetsSides.Top
+        ),
+//        modifier = Modifier.padding(bottom = 0.dp),
+//        topBar = {
+//            TopAppBar(
+//                modifier = Modifier.background(Color.Red),
+//                title = {
+//                    Text("Jobs")
+//                },
+//                actions = {
+//                    IconButton(onClick = { viewModel.refreshJobs() }) {
+//                        Icon(
+//                            Lucide.RefreshCcw,
+//                            contentDescription = null
+//                        )
+//                    }
+//                }
+//            )
+//        },
         floatingActionButton = {
             if (uiState.session.isCustomer()) {
                 FloatingActionButton(
@@ -90,43 +106,89 @@ fun JobsScreen(
             }
         },
     ) {
-        LazyColumn(modifier = Modifier.padding(it)) {
-            item {
-                // loading indicator
-                AnimatedVisibility(visible = isLoading) {
-                    LinearProgressIndicator(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 8.dp, end = 8.dp, bottom = 16.dp),
-                        strokeCap = StrokeCap.Round
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            MaterialTheme.colorScheme.surfaceContainerHigh,
+                            MaterialTheme.colorScheme.surfaceContainerLowest,
+                        )
+                    )
+                )
+                .padding(it)
+        ) {
+            // app bar
+            Row(
+                modifier = Modifier.padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 16.dp
+                ),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(text = "Jobs", style = MaterialTheme.typography.titleLarge)
+                    Text(
+                        text = "Berikut adalah beberapa pekerjaan yang dapat Anda lamar sebagai driver",
+                        style = MaterialTheme.typography.bodyMedium
                     )
                 }
+                Spacer(modifier = Modifier.width(16.dp))
+                CustomIconButton(icon = Lucide.RefreshCw, onClick = {})
             }
 
-            if (!isLoading && uiState.detail is JobsState.Detail.Success) {
-                itemsIndexed((uiState.detail as JobsState.Detail.Success).result.jobs) { index, job ->
-                    JobListItem(
-                        modifier = Modifier
-                            .padding(
-                                start = 16.dp,
-                                end = 16.dp,
-                                top = if (index > 0) 8.dp else 0.dp
-                            )
-                            .clickable {
-                                navController.navigate(
-                                    JobRoutes.Detail(
-                                        id = job.id,
-                                        type = job.type
-                                    )
+            // loading indicator
+            AnimatedVisibility(visible = isLoading) {
+                LinearProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+                    strokeCap = StrokeCap.Round
+                )
+            }
+
+            AnimatedVisibility(visible = listState.canScrollBackward) {
+                HorizontalDivider()
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f),
+                state = listState
+            ) {
+                if (!isLoading && uiState.detail is JobsState.Detail.Success) {
+                    itemsIndexed((uiState.detail as JobsState.Detail.Success).result.jobs) { index, job ->
+                        JobListItem(
+                            modifier = Modifier
+                                .padding(
+                                    start = 16.dp,
+                                    end = 16.dp,
+                                    top = if (index > 0) 8.dp else 0.dp
                                 )
-                            },
-                        customerName = job.customer.name,
-                        title = job.title,
-                        note = job.note,
-                        type = job.type,
-                        pickupLocation = job.pickupLocation,
-                        destination = job.destination
-                    )
+                                .clickable {
+                                    navController.navigate(
+                                        JobRoutes.Detail(
+                                            id = job.id,
+                                            type = job.type
+                                        )
+                                    )
+                                },
+                            customerName = job.customer.name,
+                            title = job.title,
+                            note = job.note,
+                            type = job.type,
+                            pickupLocation = job.pickupLocation,
+                            destination = job.destination
+                        )
+                    }
+
+                    item {
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
                 }
             }
         }
