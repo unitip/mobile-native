4da08bf4f084cc238544426b90ab015b1834f438
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-12 15:19:02 +0700

    refactor: simplify MainActivity by introducing Application composable and update HomeScreen start destination

--- app/src/main/java/com/unitip/mobile/MainActivity.kt
+++ app/src/main/java/com/unitip/mobile/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.navigation.compose.rememberNavController
-import com.unitip.mobile.shared.commons.ApplicationNavigationGraph
-import com.unitip.mobile.shared.commons.compositional.LocalNavController
+import com.unitip.mobile.shared.commons.Application
 import com.unitip.mobile.shared.data.managers.SessionManager
-import com.unitip.mobile.shared.presentation.ui.theme.UnitipTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -23,24 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val isAuthenticated = sessionManager.read() != null
-
         setContent {
-            UnitipTheme(
-                darkTheme = false,
-                dynamicColor = false,
-            ) {
-                Surface {
-                    val navController = rememberNavController()
-
-                    CompositionLocalProvider(LocalNavController provides navController) {
-                        ApplicationNavigationGraph(
-                            navController = navController,
-                            isAuthenticated = isAuthenticated,
-                        )
-                    }
-                }
-            }
+            Application(isAuthenticated = sessionManager.read() != null)
         }
     }
 }
---
+++ app/src/main/java/com/unitip/mobile/shared/commons/Application.kt
@@ -0,0 +1,27 @@
+package com.unitip.mobile.shared.commons
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.navigation.compose.rememberNavController
+import com.unitip.mobile.shared.commons.compositional.LocalNavController
+import com.unitip.mobile.shared.presentation.ui.theme.UnitipTheme
+
+@Composable
+fun Application(isAuthenticated: Boolean) {
+    UnitipTheme(
+        darkTheme = true,
+        dynamicColor = false,
+    ) {
+        Surface {
+            val navController = rememberNavController()
+
+            CompositionLocalProvider(LocalNavController provides navController) {
+                ApplicationNavigationGraph(
+                    navController = navController,
+                    isAuthenticated = isAuthenticated,
+                )
+            }
+        }
+    }
+}
