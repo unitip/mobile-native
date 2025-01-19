9e960f2c72d735965e18ac2516430e8fcb553323
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-19 22:54:29 +0700

    feat: move navigation graph to commons, adjust start destination

--- app/src/main/java/com/unitip/mobile/shared/commons/ApplicationNavigationGraph.kt
+++ app/src/main/java/com/unitip/mobile/shared/commons/ApplicationNavigationGraph.kt
@@ -1,4 +1,4 @@
-package com.unitip.mobile.shared.presentation.navigation
+package com.unitip.mobile.shared.commons
 
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.runtime.Composable
@@ -7,14 +7,13 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.unitip.mobile.features.auth.commons.AuthRoutes
 import com.unitip.mobile.features.auth.commons.authNavigation
-import com.unitip.mobile.features.chat.commons.ChatRoutes
 import com.unitip.mobile.features.chat.commons.chatNavigation
 import com.unitip.mobile.features.example.utils.exampleNavigation
+import com.unitip.mobile.features.home.commons.HomeRoutes
 import com.unitip.mobile.features.home.commons.homeNavigation
 import com.unitip.mobile.features.job.commons.jobNavigation
 import com.unitip.mobile.features.offer.commons.offerNavigation
 import com.unitip.mobile.features.test.TestScreen
-import com.unitip.mobile.shared.commons.Routes
 
 @Composable
 fun ApplicationNavigationGraph(
@@ -24,11 +23,12 @@ fun ApplicationNavigationGraph(
     NavHost(
         navController = navController,
         startDestination = when (isAuthenticated) {
-//            true -> HomeRoutes.Index
-            true -> ChatRoutes.Conversation(
-                roomId = "b48fc434-6b59-4dfb-9b22-8ba4d27068ac",
-                otherUserName = "Rizal Dwi Anggoro"
-            )
+            true -> HomeRoutes.Index
+//            true -> ChatRoutes.Conversation(
+//                roomId = "b48fc434-6b59-4dfb-9b22-8ba4d27068ac",
+//                otherUserId = "1cd709cb-3e77-4376-b080-b225174d8e9a",
+//                otherUserName = "Hehehehe"
+//            )
 
             false -> AuthRoutes.Index
         },
