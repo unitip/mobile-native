5d4696534215c8c97a1ab8858a306cd9c34fe1d8
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-19 22:31:39 +0700

    feat: add mqtt topic for driver online status

--- app/src/main/java/com/unitip/mobile/shared/commons/configs/MqttTopics.kt
+++ app/src/main/java/com/unitip/mobile/shared/commons/configs/MqttTopics.kt
@@ -5,6 +5,14 @@ import com.unitip.mobile.BuildConfig
 object MqttTopics {
     private const val PREFIX = "com.unitip/${BuildConfig.MQTT_SECRET}"
 
+    object Dashboard {
+        fun publishDriverOnlineStatus(driverUserId: String): String =
+            "$PREFIX/dashboard/driver/$driverUserId/status"
+
+        fun subscribeDriverOnlineStatus(): String =
+            "$PREFIX/dashboard/driver/+/status"
+    }
+
     object Chats {
         fun publishSubscribeMessage(roomId: String): String =
             "$PREFIX/chats/message/$roomId"
