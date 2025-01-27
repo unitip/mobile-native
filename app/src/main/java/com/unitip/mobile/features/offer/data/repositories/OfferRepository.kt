2f9301d2fbbb9053f45bcd9d0b4855669128b8e6
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-27 11:44:55 +0700

    fix: import error

--- app/src/main/java/com/unitip/mobile/features/offer/data/repositories/OfferRepository.kt
+++ app/src/main/java/com/unitip/mobile/features/offer/data/repositories/OfferRepository.kt
@@ -1,7 +1,6 @@
 package com.unitip.mobile.features.offer.data.repositories
 
 import arrow.core.Either
-import com.unitip.mobile.features.job.domain.models.GetAllJobsResult
 import com.unitip.mobile.features.offer.data.dtos.CreateOfferPayload
 import com.unitip.mobile.features.offer.data.dtos.GetAllOfferResponse
 import com.unitip.mobile.features.offer.data.models.CreateOfferResult
@@ -12,15 +11,14 @@ import com.unitip.mobile.features.offer.domain.models.OfferFreelancer
 import com.unitip.mobile.shared.commons.extensions.mapToFailure
 import com.unitip.mobile.shared.data.managers.SessionManager
 import com.unitip.mobile.shared.domain.models.Failure
-import kotlinx.coroutines.Job
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class OfferRepository @Inject constructor(
     private val sessionManager: SessionManager,
-    private val offerApi:OfferApi
-){
+    private val offerApi: OfferApi
+) {
     suspend fun create(
         title: String,
         description: String,
@@ -29,11 +27,11 @@ class OfferRepository @Inject constructor(
         pickupArea: String,
         deliveryArea: String,
         availableUntil: String
-    ):Either<Failure,CreateOfferResult>{
+    ): Either<Failure, CreateOfferResult> {
         try {
             val token = sessionManager.read()?.token
             val response = offerApi.create(
-                token="Bearer $token",
+                token = "Bearer $token",
                 payload = CreateOfferPayload(
                     title = title,
                     description = description,
@@ -47,49 +45,52 @@ class OfferRepository @Inject constructor(
 
             val result = response.body()
             // mereturn jika sukses dan jika tidak
-           return when (response.isSuccessful && result != null){
-               true -> Either.Right(CreateOfferResult(id = result.id))
-               false -> Either.Left(response.mapToFailure())
-           }
-        }catch (e: Exception){
+            return when (response.isSuccessful && result != null) {
+                true -> Either.Right(CreateOfferResult(id = result.id))
+                false -> Either.Left(response.mapToFailure())
+            }
+        } catch (e: Exception) {
             return Either.Left(Failure(message = "Terjadi kesalahan tak terduga!"))
         }
     }
 
 
-    suspend fun getAll() : Either<Failure,GetAllOffersResult>{
-    try {
-        val token = sessionManager.read()?.token
-        val response = offerApi.getAll(token="Bearer $token", type = GetAllOfferResponse.PageInfo.OfferType.SINGLE.value)
-        val result = response.body()
-
-        return when (response.isSuccessful && result != null){
-            true -> Either.Right(
-                GetAllOffersResult(
-                    offers = result.offers.map {
-                        Offer(
-                            id = it.id,
-                            title = it.title,
-                            description = it.description,
-                            price = it.price,
-                            type = it.type,
-                            pickupArea = it.pickupArea,
-                            deliveryArea = it.deliveryArea,
-                            availableUntil = it.availableUntil,
-                            offerStatus = it.offerStatus,
-                            freelancer = OfferFreelancer(
-                                name = it.freelancer.name
-                            )
-                        )
-                    },
-                    hasNext = result.pageInfo.page < result.pageInfo.totalPages
-                )
+    suspend fun getAll(): Either<Failure, GetAllOffersResult> {
+        try {
+            val token = sessionManager.read()?.token
+            val response = offerApi.getAll(
+                token = "Bearer $token",
+                type = GetAllOfferResponse.PageInfo.OfferType.SINGLE.value
             )
+            val result = response.body()
 
-            false -> Either.Left(response.mapToFailure())
-        }
+            return when (response.isSuccessful && result != null) {
+                true -> Either.Right(
+                    GetAllOffersResult(
+                        offers = result.offers.map {
+                            Offer(
+                                id = it.id,
+                                title = it.title,
+                                description = it.description,
+                                price = it.price,
+                                type = it.type,
+                                pickupArea = it.pickupArea,
+                                deliveryArea = it.deliveryArea,
+                                availableUntil = it.availableUntil,
+                                offerStatus = it.offerStatus,
+                                freelancer = OfferFreelancer(
+                                    name = it.freelancer.name
+                                )
+                            )
+                        },
+                        hasNext = result.pageInfo.page < result.pageInfo.totalPages
+                    )
+                )
 
-        } catch (e: Exception){
+                false -> Either.Left(response.mapToFailure())
+            }
+
+        } catch (e: Exception) {
             e.printStackTrace()
             return Either.Left(Failure(message = "Terjadi kesalahan tak terduga !"))
         }
--- app/src/main/java/com/unitip/mobile/features/offer/domain/models/GetAllOffersResult.kt
+++ app/src/main/java/com/unitip/mobile/features/offer/domain/models/GetAllOffersResult.kt
@@ -1,7 +1,5 @@
 package com.unitip.mobile.features.offer.domain.models
 
-import com.unitip.mobile.features.job.domain.models.Job
-
 data class GetAllOffersResult(
     val offers: List<Offer> = emptyList(),
     val hasNext: Boolean = false
