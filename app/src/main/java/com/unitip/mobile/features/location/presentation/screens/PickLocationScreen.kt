c4f8d4169700d25f2ab3b19e3aad202664355ea5
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-28 17:45:17 +0700

    feat: add location screen
    - Add PickLocationState and PickLocationViewModel for managing location data
    - Implement PickLocationScreen with map view and marker
    - Add functionality to get and display last location
    - Add zoom in and zoom out buttons
    - Add current location coordinate display

--- app/src/main/java/com/unitip/mobile/features/location/presentation/screens/PickLocationScreen.kt
+++ app/src/main/java/com/unitip/mobile/features/location/presentation/screens/PickLocationScreen.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,21 +28,22 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.composables.icons.lucide.ChevronLeft
+import com.composables.icons.lucide.LocateFixed
 import com.composables.icons.lucide.Lucide
 import com.composables.icons.lucide.MapPin
+import com.composables.icons.lucide.ZoomIn
+import com.composables.icons.lucide.ZoomOut
+import com.unitip.mobile.features.location.presentation.components.rememberMapView
+import com.unitip.mobile.features.location.presentation.states.PickLocationState
+import com.unitip.mobile.features.location.presentation.viewmodels.PickLocationViewModel
 import com.unitip.mobile.shared.commons.compositional.LocalNavController
 import com.unitip.mobile.shared.presentation.components.CustomIconButton
-import org.osmdroid.events.MapListener
-import org.osmdroid.events.ScrollEvent
-import org.osmdroid.events.ZoomEvent
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.Polygon
 
 val unsGeoPoint = GeoPoint(-7.559843, 110.856658)
 
@@ -48,10 +52,40 @@ fun PickLocationScreen(
     resultKey: String,
     initialLatitude: Double? = null,
     initialLongitude: Double? = null,
+    viewModel: PickLocationViewModel = hiltViewModel()
 ) {
     val navController = LocalNavController.current
+    val context = LocalContext.current
 
-    var currentGeoPoint by remember { mutableStateOf(GeoPoint(0.0, 0.0)) }
+    var currentGeoPoint by remember { mutableStateOf(unsGeoPoint) }
+    val uiState by viewModel.uiState.collectAsState()
+
+    val mapView = rememberMapView(
+        context = context,
+        initialGeoPoint = when (initialLatitude != null && initialLongitude != null) {
+            true -> GeoPoint(initialLatitude, initialLongitude)
+            else -> null
+        },
+        onChangeGeoPoint = {
+            currentGeoPoint = it
+        }
+    )
+
+    with(uiState.getLastLocationDetail) {
+        when (this) {
+            is PickLocationState.GetLastLocationDetail.SuccessGetLastLocation -> {
+                mapView.controller.animateTo(
+                    GeoPoint(
+                        lastLocation.latitude,
+                        lastLocation.longitude
+                    )
+                )
+                viewModel.resetLastLocationState()
+            }
+
+            else -> Unit
+        }
+    }
 
     Scaffold {
         Column(
@@ -80,7 +114,13 @@ fun PickLocationScreen(
                         navController.popBackStack()
                     }
                 )
-                Text(text = "Pilih Lokasi", style = MaterialTheme.typography.titleLarge)
+                Column {
+                    Text(text = "Pilih Lokasi", style = MaterialTheme.typography.titleLarge)
+                    Text(
+                        text = "${currentGeoPoint.latitude},${currentGeoPoint.longitude}",
+                        style = MaterialTheme.typography.labelSmall
+                    )
+                }
             }
             HorizontalDivider()
 
@@ -92,72 +132,78 @@ fun PickLocationScreen(
             ) {
                 // mapview
                 AndroidView(
-                    factory = { context ->
-                        val mapView = MapView(context).apply {
-                            setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
-                            setMultiTouchControls(true)
-
-                            controller.setCenter(
-                                when (initialLatitude != null && initialLongitude != null) {
-                                    true -> GeoPoint(initialLatitude, initialLongitude)
-                                    else -> unsGeoPoint
-                                }
-                            )
-                            controller.setZoom(18.5)
-                        }
-                        mapView.addMapListener(object : MapListener {
-                            override fun onScroll(event: ScrollEvent?): Boolean {
-                                currentGeoPoint = GeoPoint(
-                                    mapView.mapCenter.latitude,
-                                    mapView.mapCenter.longitude
-                                )
-                                return true
-                            }
-
-                            override fun onZoom(event: ZoomEvent?): Boolean {
-                                currentGeoPoint = GeoPoint(
-                                    mapView.mapCenter.latitude,
-                                    mapView.mapCenter.longitude
-                                )
-                                return true
-                            }
-                        })
-
+                    factory = {
                         // circle zone radius
-                        val circle = Polygon().apply {
-                            points = Polygon.pointsAsCircle(unsGeoPoint, 2000.0)
-                            outlinePaint.strokeWidth = 4f
-                            outlinePaint.color = Color.Red.toArgb()
-                        }
-                        mapView.overlays.add(circle)
-
-                        val circle2 = Polygon().apply {
-                            points = Polygon.pointsAsCircle(unsGeoPoint, 4000.0)
-                            outlinePaint.strokeWidth = 4f
-                            outlinePaint.color = Color.Blue.toArgb()
-                        }
-                        mapView.overlays.add(circle2)
+//                        val circle = Polygon().apply {
+//                            points = Polygon.pointsAsCircle(unsGeoPoint, 2000.0)
+//                            outlinePaint.strokeWidth = 4f
+//                            outlinePaint.color = Color.Red.toArgb()
+//                        }
+//                        mapView.overlays.add(circle)
+//
+//                        val circle2 = Polygon().apply {
+//                            points = Polygon.pointsAsCircle(unsGeoPoint, 4000.0)
+//                            outlinePaint.strokeWidth = 4f
+//                            outlinePaint.color = Color.Blue.toArgb()
+//                        }
+//                        mapView.overlays.add(circle2)
 
                         mapView
                     }
                 )
 
-                // marker penanda
-                Icon(
-                    Lucide.MapPin,
-                    contentDescription = null,
-                    tint = Color.Red,
-                    modifier = Modifier.align(Alignment.Center)
-                )
+                Box(modifier = Modifier.fillMaxSize()) {
+                    // marker penanda
+                    Icon(
+                        Lucide.MapPin,
+                        contentDescription = null,
+                        tint = Color.Red,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+
+                    // button get last location
+                    Column(
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(end = 16.dp, bottom = 16.dp)
+                    ) {
+                        FilledTonalIconButton(
+                            onClick = {
+                                viewModel.getLastLocation()
+                            }) {
+                            Icon(
+                                Lucide.LocateFixed,
+                                contentDescription = null,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+                        FilledTonalIconButton(
+                            onClick = {
+                                mapView.controller.zoomIn()
+                            }
+                        ) {
+                            Icon(
+                                Lucide.ZoomIn,
+                                contentDescription = null,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+                        FilledTonalIconButton(
+                            onClick = {
+                                mapView.controller.zoomOut()
+                            }
+                        ) {
+                            Icon(
+                                Lucide.ZoomOut,
+                                contentDescription = null,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+                    }
+                }
             }
 
             HorizontalDivider()
-
-            Text(
-                text = "${currentGeoPoint.latitude},${currentGeoPoint.longitude}",
-                style = MaterialTheme.typography.bodySmall,
-                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
-            )
 
             // button confirm
             Button(
