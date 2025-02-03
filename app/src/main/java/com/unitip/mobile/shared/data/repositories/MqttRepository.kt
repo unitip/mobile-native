9f87aa7a22e8e844135c5797fd66395731deb07c
Rizal Anggoro <gnoogler4@gmail.com>
2025-02-03 11:38:35 +0700

    refactor: move driver online status logic out of session check
    The diff modifies the `MqttRepository` to initialize MQTT topics and connection options regardless of the session presence. The logic for publishing driver online/offline status is moved outside the session check to ensure it's always configured when connecting to the MQTT broker. The `isCleanSession` option is also explicitly set to `false`.

--- app/src/main/java/com/unitip/mobile/shared/data/repositories/MqttRepository.kt
+++ app/src/main/java/com/unitip/mobile/shared/data/repositories/MqttRepository.kt
@@ -28,67 +28,67 @@ class MqttRepository @Inject constructor(
          * mendapatkan session dari user yang sedang login saat ini
          */
         val session = sessionManager.read()
-        if (session != null) {
+
+        /**
+         * inisialisasi beberapa topics mqtt yang akan digunakan
+         */
+        val publishDriverOnlineStatusTopic = MqttTopics.Dashboard.publishDriverOnlineStatus(
+            driverUserId = session.id
+        )
+
+        val options = MqttConnectOptions().apply {
+            isAutomaticReconnect = true
+            isCleanSession = false
+
             /**
-             * inisialisasi beberapa topics mqtt yang akan digunakan
+             * ketika user login sebagai driver, maka status online dan offline
+             * dari user tersebut akan dicatat dan disimpan pada broker mqtt
              */
-            val publishDriverOnlineStatusTopic = MqttTopics.Dashboard.publishDriverOnlineStatus(
-                driverUserId = session.id
-            )
+            if (session.isDriver())
+                setWill(
+                    publishDriverOnlineStatusTopic,
+                    "offline".toByteArray(),
+                    2,
+                    true
+                )
 
-            val options = MqttConnectOptions().apply {
-                isAutomaticReconnect = true
-
-                /**
-                 * ketika user login sebagai driver, maka status online dan offline
-                 * dari user tersebut akan dicatat dan disimpan pada broker mqtt
-                 */
-                if (session.isDriver())
-                    setWill(
-                        publishDriverOnlineStatusTopic,
-                        "offline".toByteArray(),
-                        2,
-                        true
-                    )
-
-                /**
-                 * setiap user yang login dapat mengirimkan pesan dan memiliki status
-                 * typing, sehingga perlu mereset status typing tersebut dari broker
-                 * ketika user kehilangan koneksi
-                 */
+            /**
+             * setiap user yang login dapat mengirimkan pesan dan memiliki status
+             * typing, sehingga perlu mereset status typing tersebut dari broker
+             * ketika user kehilangan koneksi
+             */
 //                setWill(
 //                    MqttTopics.Chats.publishTypingStatus(currentUserId = session.id),
 //                    "".toByteArray(), // untuk menghapus retained, maka kirim empty string
 //                    2,
 //                    true
 //                )
-            }
-
-            client.connect(
-                options = options,
-                userContext = null,
-                callback = object : IMqttActionListener {
-                    override fun onSuccess(asyncActionToken: IMqttToken?) {
-                        Log.d(TAG, "onSuccess: connected")
-
-                        /**
-                         * ketika user login sebagai driver, maka status online dan offline
-                         * dari user tersebut akan dicatat dan disimpan pada broker mqtt
-                         */
-                        if (session.isDriver())
-                            client.publish(
-                                publishDriverOnlineStatusTopic,
-                                "online".toByteArray(),
-                                2,
-                                true
-                            )
-                    }
-
-                    override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                        exception?.printStackTrace()
-                        Log.d(TAG, "onFailure: ${exception?.message ?: "unknown"}")
-                    }
-                })
         }
+
+        client.connect(
+            options = options,
+            userContext = null,
+            callback = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(TAG, "onSuccess: connected")
+
+                    /**
+                     * ketika user login sebagai driver, maka status online dan offline
+                     * dari user tersebut akan dicatat dan disimpan pada broker mqtt
+                     */
+                    if (session.isDriver())
+                        client.publish(
+                            publishDriverOnlineStatusTopic,
+                            "online".toByteArray(),
+                            2,
+                            true
+                        )
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    exception?.printStackTrace()
+                    Log.d(TAG, "onFailure: ${exception?.message ?: "unknown"}")
+                }
+            })
     }
 }
