dce1b56d6bba13baba92c583343392c210415b3c
Rizal Anggoro <gnoogler4@gmail.com>
2025-02-05 09:37:14 +0700

    feat: add refresh token on application start
    The application now refreshes the token on startup using the `SessionManager`.

--- app/src/main/java/com/unitip/mobile/shared/commons/UnitipApplication.kt
+++ app/src/main/java/com/unitip/mobile/shared/commons/UnitipApplication.kt
@@ -1,7 +1,21 @@
 package com.unitip.mobile.shared.commons
 
 import android.app.Application
+import com.unitip.mobile.shared.commons.configs.ApiConfig
+import com.unitip.mobile.shared.data.managers.SessionManager
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class UnitipApplication : Application()
+class UnitipApplication : Application() {
+    @Inject
+    lateinit var sessionManager: SessionManager
+
+    override fun onCreate() {
+        super.onCreate()
+
+        ApiConfig.refreshToken(
+            token = sessionManager.getToken()
+        )
+    }
+}
