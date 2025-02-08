91eb68d96f9946cfc0a5c621eee8d3293af265cc
Rizal Anggoro <gnoogler4@gmail.com>
2025-02-08 09:30:57 +0700

    refactor: migrate to generated auth api
    - Migrate to use generated auth api.
    - Deprecated old auth dto & auth api.
    - Removed `AuthApi`, `LoginPayload`, `LoginResponse`, `RegisterPayload`, and `RegisterResponse`.

--- app/src/main/java/com/unitip/mobile/features/auth/data/dtos/LoginPayload.kt
+++
@@ -1,8 +0,0 @@
-package com.unitip.mobile.features.auth.data.dtos
-
-@Deprecated("Gunakan hasil generated dari openapi")
-data class LoginPayload(
-    val email: String,
-    val password: String,
-    val role: String? = null,
-)
--- app/src/main/java/com/unitip/mobile/features/auth/data/dtos/RegisterPayload.kt
+++
@@ -1,8 +0,0 @@
-package com.unitip.mobile.features.auth.data.dtos
-
-@Deprecated("Gunakan hasil generated dari openapi")
-data class RegisterPayload(
-    val name: String,
-    val email: String,
-    val password: String,
-)
