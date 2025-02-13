1eb98fc1d4e7b1767647dab539a6142de42eee07
Rizal Anggoro <gnoogler4@gmail.com>
2025-02-13 09:32:31 +0700

    feat: add GenderConstant enum and update Session model
    - Introduce `GenderConstant` enum for specifying gender options.
    - Update `Session` model to use `GenderConstant` enum instead of string for gender.

--- app/src/main/java/com/unitip/mobile/shared/domain/models/Session.kt
+++ app/src/main/java/com/unitip/mobile/shared/domain/models/Session.kt
@@ -1,10 +1,12 @@
 package com.unitip.mobile.shared.domain.models
 
+import com.unitip.mobile.shared.commons.constants.GenderConstant
+
 data class Session(
     val id: String = "",
     val name: String = "",
     val email: String = "",
     val token: String = "",
     val role: String = "",
-    val gender: String = ""
+    val gender: GenderConstant = GenderConstant.NotSpecified
 )
