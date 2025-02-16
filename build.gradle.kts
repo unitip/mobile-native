78207a3f4bcf8884f691f3689a55c298be4e6220
rizalanggoro <gnoogler4@gmail.com>
2025-02-17 06:52:58 +0700

    feat: update kotlin and ksp versions
    The Kotlin version has been updated from 2.0.20 to 2.1.10. The Kotlin Symbol Processing (KSP) version has also been updated from 2.0.21-1.0.27 to 2.1.10-1.0.30.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.30" apply false
     id("com.google.dagger.hilt.android") version "2.53" apply false
 
     id("org.openapi.generator") version "7.10.0" apply false
