4d6b345ce94cf65c77b92648af53f669e6014a03
rizalanggoro <gnoogler4@gmail.com>
2025-02-17 14:36:02 +0700

    feat: refactor job creation screen and update create job request
    -   Refactored the CreateJobScreen to improve UI/UX.
    -   Replaced custom service selection with a ServiceTypeDropdown.
    -   Added circular progress indicator during job creation.
    -   Updated CreateJobRequest to use ServiceTypeConstant enum.
    -   Removed unused imports and code.
    -   Refactor createJobViewModel for update state.

--- app/src/main/java/com/unitip/mobile/features/job/presentation/viewmodels/CreateJobViewModel.kt
+++ app/src/main/java/com/unitip/mobile/features/job/presentation/viewmodels/CreateJobViewModel.kt
@@ -2,36 +2,34 @@ package com.unitip.mobile.features.job.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.unitip.mobile.features.job.commons.JobConstant
 import com.unitip.mobile.features.job.data.repositories.CustomerJobRepository
-import com.unitip.mobile.features.job.presentation.states.CreateJobState
+import com.unitip.mobile.shared.commons.constants.ServiceTypeConstant
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import com.unitip.mobile.features.job.presentation.states.CreateJobState as State
 
 @HiltViewModel
 class CreateJobViewModel @Inject constructor(
     private val customerJobRepository: CustomerJobRepository
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow(CreateJobState())
+    private val _uiState = MutableStateFlow(State())
     val uiState get() = _uiState
 
-    fun resetState() {
-        _uiState.value = with(uiState.value) {
-            copy(detail = CreateJobState.Detail.Initial)
-        }
+    fun resetState() = _uiState.update {
+        it.copy(detail = State.Detail.Initial)
     }
 
     fun create(
         note: String,
         pickupLocation: String,
         destinationLocation: String,
-        service: JobConstant.Service,
+        service: ServiceTypeConstant,
         expectedPrice: Int
     ) = viewModelScope.launch {
-        _uiState.update { it.copy(detail = CreateJobState.Detail.Loading) }
+        _uiState.update { it.copy(detail = State.Detail.Loading) }
         customerJobRepository
             .create(
                 note = note,
@@ -43,7 +41,7 @@ class CreateJobViewModel @Inject constructor(
             .onLeft { left ->
                 _uiState.update {
                     it.copy(
-                        detail = CreateJobState.Detail.Failure(
+                        detail = State.Detail.Failure(
                             message = left.message
                         )
                     )
@@ -51,7 +49,7 @@ class CreateJobViewModel @Inject constructor(
             }
             .onRight {
                 _uiState.update {
-                    it.copy(detail = CreateJobState.Detail.Success)
+                    it.copy(detail = State.Detail.Success)
                 }
             }
     }
